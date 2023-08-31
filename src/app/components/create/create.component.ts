bcf61d460733ab1012a5e8dad4a1f7f5a5dc3064
Lucas-Vfo <fabian.flores.oyarce@alumnos.uta.cl>
2023-08-31 12:46:22 -0400

    Agregar empleado

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import {EmployeesService} from '../../services/employees.service';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css']
 })
-export class CreateComponent {
 
+export class CreateComponent {
+  emp?: any;
+  constructor (private employeesService: EmployeesService) {
+    this.emp = this.employeesService.getEmployees();
+  }
 }
